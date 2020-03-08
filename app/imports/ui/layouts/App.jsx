be45ed160918d1f8a16cf75764c9894f73b2750d
In Woo Park <48573161+inwooCS@users.noreply.github.com>
2020-03-08 12:22:50 -1000

    Task 4: Finished

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -7,10 +7,6 @@ import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import TopMenu from '../components/TopMenu';
 import Footer from '../components/Footer';
 import YourChoice from '../pages/YourChoice';
-import ListStuff from '../pages/ListStuff';
-import ListStuffAdmin from '../pages/ListStuffAdmin';
-import AddStuff from '../pages/AddStuff';
-import EditStuff from '../pages/EditStuff';
 import NotFound from '../pages/NotFound';
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
--- app/imports/ui/pages/YourChoice.jsx
+++ app/imports/ui/pages/YourChoice.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import TopMenu from '../components/TopMenu';
 import PageImage from '../components/PageImage';
 import BottomImage from '../components/BottomImage';
-import Footer from '../components/Footer';
 
 export default class IslandSnow extends React.Component {
 
